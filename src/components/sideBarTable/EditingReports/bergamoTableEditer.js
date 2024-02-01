d02c8d93c403fe2d11c71a5ce928b8c6125b31e2
melatmesele <94954601+melatmesele@users.noreply.github.com>
2024-02-01 12:32:34 +0300

    Update bergamoTableEditer.js

--- src/components/sideBarTable/EditingReports/bergamoTableEditer.js
+++ src/components/sideBarTable/EditingReports/bergamoTableEditer.js
@@ -1,7 +1,6 @@
-
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
-import { useSelector,useDispatch} from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import {
   setTotalsReport,
   setPersonalExpenseReport,
@@ -12,96 +11,79 @@ import {
   getReportData,
 } from "../../../api-helper-function/apiCallerFunction";
 
-
 const EditedBergamoTable = () => {
-  
-  const minDate= useSelector((state) => state.reportData.minDate);
+  const minDate = useSelector((state) => state.reportData.minDate);
   const maxDate = useSelector((state) => state.reportData.maxDate);
   const [selectedDate, setSelectedDate] = useState(null);
   const [bergamod, setBergamod] = useState(0);
   const bergamoData = useSelector((state) => state.reportData.totalsReport);
-  const selectedSprintId = useSelector(
-    (state) => state.reportData.sprintIdd
-  );
-
-
+  const selectedSprintId = useSelector((state) => state.reportData.sprintIdd);
 
   const [sold, setSold] = useState(0);
-    const formatSelectedDate = (date) => {
+  const formatSelectedDate = (date) => {
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Adding 1 because getMonth() returns 0-based month
     const day = date.getDate().toString().padStart(2, "0");
     return `${year}-${month}-${day}`;
-    };
+  };
   const dispatch = useDispatch();
-    
-    const handleBergamoReportData = async () => {
-      if (selectedDate !== null) {
-        // Format the selected date to match the foam data format
-        const formattedDate = formatSelectedDate(selectedDate);
 
-        // Find the corresponding ID for the selected date
-        const selectedId = bergamoData.find(
-          (item) => item.date === formattedDate
-        )?.id;
-        // console.log(formattedDate);
+  const handleBergamoReportData = async () => {
+    if (selectedDate !== null) {
+      // Format the selected date to match the foam data format
+      const formattedDate = formatSelectedDate(selectedDate);
 
-        if (selectedId) {
-          try {
-            const response = await addBergamoData(selectedId, sold, bergamod);
-            // Update the foamData state with the modified data
-            const updatedBergamoData = bergamoData.map((item) =>
-              item.id === selectedId
-                ? {
-                    ...item,
-                    sold: response.sold,
-                    bergamod: response.bergamod,
-                  }
-                : item
-            );
-            dispatch(setTotalsReport(updatedBergamoData));
-            const reportData = await getReportData(selectedSprintId);
+      // Find the corresponding ID for the selected date
+      const selectedId = bergamoData.find(
+        (item) => item.date === formattedDate
+      )?.id;
+      // console.log(formattedDate);
 
-            dispatch(setPersonalExpenseReport(reportData.PersonalProfit));
-            dispatch(setExpenseReport(reportData.net));
+      if (selectedId) {
+        try {
+          const response = await addBergamoData(selectedId, sold, bergamod);
+          // Update the foamData state with the modified data
+          const updatedBergamoData = bergamoData.map((item) =>
+            item.id === selectedId
+              ? {
+                  ...item,
+                  sold: response.sold,
+                  bergamod: response.bergamod,
+                }
+              : item
+          );
+          dispatch(setTotalsReport(updatedBergamoData));
+          const reportData = await getReportData(selectedSprintId);
 
-      
-          } catch (error) {
-            console.error("Error sending data to the backend:", error);
-          }
-        } else {
-          console.error("No id found for the selected date");
+          dispatch(setPersonalExpenseReport(reportData.PersonalProfit));
+          dispatch(setExpenseReport(reportData.net));
+        } catch (error) {
+          console.error("Error sending data to the backend:", error);
         }
       } else {
-        console.error(
-          "Please select a date before sending data to the backend"
-        );
+        console.error("No id found for the selected date");
+      }
+    } else {
+      console.error("Please select a date before sending data to the backend");
+    }
+  };
+  useEffect(() => {
+    const fetchDataForSelectedSprint = async () => {
+      try {
+        const reportData = await getReportData(selectedSprintId);
+
+        dispatch(setPersonalExpenseReport(reportData.PersonalProfit));
+        dispatch(setExpenseReport(reportData.net));
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
     };
- useEffect(() => {
-   const fetchDataForSelectedSprint = async () => {
-     try {
 
-       const reportData = await getReportData(selectedSprintId);
-       
-       
-
-      
-       
-       dispatch(setPersonalExpenseReport(reportData.PersonalProfit));
-       dispatch(setExpenseReport(reportData.net));
-     } catch (error) {
-       console.error("Error fetching data:", error);
-     }
-   };
-
-   fetchDataForSelectedSprint();
- }, [selectedSprintId, dispatch]);
-
-
+    fetchDataForSelectedSprint();
+  }, [selectedSprintId, dispatch]);
   return (
     <div className="max-w-md bg-white">
-      <form
+      <div
         className="flex flex-row items-center justify-between space-x-4"
         action="#"
         method="POST"
@@ -144,10 +126,9 @@ const EditedBergamoTable = () => {
         >
           Save
         </button>
-      </form>
+      </div>
     </div>
   );
+};
 
-}
-
-export default EditedBergamoTable
+export default EditedBergamoTable;
