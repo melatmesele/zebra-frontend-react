3118368d9fba745084f4e5b7517bfa64397ff4bc
melatmesele <21melmes@gmail.com>
2024-02-04 18:31:50 +0300

    logo added

--- src/components/HomePage/navBar.js
+++ src/components/HomePage/navBar.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { useNavigate, Link, useLocation } from "react-router-dom"; // Import Link here
+import { useNavigate, Link, useLocation } from "react-router-dom"; 
 import { useDispatch } from "react-redux";
-import { setToken, setIsLoggedIn } from "../store/loginSlice"; // Update with correct path
-import { Logout } from "../../api-helper-function/apiCallerFunction"; // Update with correct path
-import Logo from "./logo"; // Ensure this import path is correct
+import { setToken, setIsLoggedIn } from "../store/loginSlice"; 
+import { Logout } from "../../api-helper-function/apiCallerFunction";
+import finalLogo from "../../assets/finalLogo.png";
 
 const NavBar = () => {
   const navigate = useNavigate();
@@ -28,10 +28,12 @@ const NavBar = () => {
     <div className="w-full fixed top-0 right-0 z-50">
       <div className="flex justify-end mr-8 md:mr-[69px] items-center justify-between">
         <div className="flex flex-row space-x-4 mt-4">
-          <Link to="/" className="w-1/5 mr-2">
-            {" "}
-            {/* Use Link here */}
-            <Logo />
+          <Link to="/" className="w-1/4 mr-2">
+            <img
+              className="transform h-full w-full p-3 rounded-lg object-cover flex justify-center"
+              src={finalLogo}
+              alt=""
+            />
           </Link>
         </div>
         <ul className="flex space-x-4 justify-end sm:space-x-8 text-md sm:text-2xl">
